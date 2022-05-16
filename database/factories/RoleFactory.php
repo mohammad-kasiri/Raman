15482b7b88927a9fb7a8217b197f021bd61d9f7c
Mohammad Kasiri <mohammad.kasirey@gmail.com>
2022-05-16 23:43:11 +0430

    Add Factory To Role Model

--- database/factories/RoleFactory.php
+++ database/factories/RoleFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Role>
@@ -17,7 +18,20 @@ class RoleFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'name'        => Str::random(10),
+            'label'       => Str::random(10),
+            'description' => Str::random(10)
         ];
+    }
+
+    public function fullAdmin()
+    {
+        return $this->state(function (array $attributes) {
+            return [
+                'name'        => 'full admin',
+                'label'       => 'مدیر ارشد',
+                'description' => 'تمامی دسترسی ها را دارد'
+            ];
+        });
     }
 }
