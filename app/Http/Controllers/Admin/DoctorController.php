4871780281dc6ec5f4e29371f4f8ea1b4afee499
Mohammad Kasiri <mohammad.kasirey@gmail.com>
2022-05-21 08:05:58 +0430

    Create Doctor Controller Template

--- app/Http/Controllers/Admin/DoctorController.php
+++ app/Http/Controllers/Admin/DoctorController.php
@@ -3,9 +3,44 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Doctor\StoreRequest;
+use App\Http\Requests\Doctor\UpdateRequest;
+use App\Models\Province;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class DoctorController extends Controller
 {
-    //
+    public function index()
+    {
+        $doctors = User::query()->doctors()->latest()->paginate(User::PAGINATION_LIMIT);;
+        return view('admin.users.doctors.index')->with(['doctors' => $doctors]);
+    }
+
+    public function create()
+    {
+        return view('');
+    }
+
+    public function store(StoreRequest $request)
+    {
+        User::query()->create();
+        return redirect()->route('');
+    }
+
+    public function edit($doctor)
+    {
+        $doctor = User::findOrFail($doctor);
+        $cities = Province::query()->cities()->get();
+
+        return view('admin.users.patients.edit')
+            ->with(['doctor' => $doctor])
+            ->with(['cities' => $cities]);
+    }
+
+    public function update(UpdateRequest $request , User $doctor)
+    {
+        $doctor->update();
+        return redirect()->route('admin.doctor.show', $doctor->id);
+    }
 }
