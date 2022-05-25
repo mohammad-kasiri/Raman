67f0e34cf8e5001c71bb01fc5f183da757079dd2
Mohammad Kasiri <mohammad.kasirey@gmail.com>
2022-05-25 08:20:59 +0430

    One To One Relation Between Doctor And User

--- app/Models/User.php
+++ app/Models/User.php
@@ -81,6 +81,11 @@ class User extends Authenticatable
         return $this->hasOne(Patient::class);
     }
 
+    public function doctor()
+    {
+        return $this->hasOne(Doctor::class);
+    }
+
     public function getFullNameAttribute()
     {
         return $this->first_name . ' ' . $this->last_name;
