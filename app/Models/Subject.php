9a79b8c7943b634896c8a2d14849d70e9918abe5
Mohammad Kasiri <mohammad.kasirey@gmail.com>
2022-05-25 08:20:15 +0430

    Rename Skill Tp Subject And Improving Migration And Model

---
+++ app/Models/Subject.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Subject extends Model
+{
+    protected $fillable=['title'];
+
+    public $timestamps = false;
+
+    public function doctors()
+    {
+        return $this->belongsToMany(Doctor::class,'doctor_Subject','subject_id','doctor_id');
+    }
+}
--- database/migrations/2022_05_23_232820_create_subjects_table.php
+++ database/migrations/2022_05_23_232820_create_subjects_table.php
@@ -13,9 +13,9 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('skills', function (Blueprint $table) {
+        Schema::create('Subjects', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string('title' , 30);
         });
     }
 
@@ -26,6 +26,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('skills');
+        Schema::dropIfExists('Subjects');
     }
 };
