4266e2a58fc5a4a5679c9f9b7d670284053c6629
Mohammad Kasiri <mohammad.kasirey@gmail.com>
2022-05-28 23:19:36 +0430

    Doctor Store Validation

--- app/Http/Requests/Doctor/StoreRequest.php
+++ app/Http/Requests/Doctor/StoreRequest.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Requests\Doctor;
 
+use App\Rules\PhoneNumber;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class StoreRequest extends FormRequest
 {
@@ -13,7 +15,7 @@ class StoreRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,7 +26,23 @@ class StoreRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'first_name'                =>      ['required' , 'min:3', 'max:15', 'bail'] ,
+            'last_name'                 =>      ['required' , 'min:3', 'max:25', 'bail'] ,
+            'mobile'                    =>      ['required' , new PhoneNumber() , 'unique:users,mobile' , 'bail'] ,
+            'gender'                    =>      ['required' , Rule::in(['male' , 'female']), 'bail'],
+            'email'                     =>      ['nullable' , 'email' , 'unique:users,email' , 'bail'],
+            'password'                  =>      ['nullable' , 'min:4' , 'max:20' , 'confirmed'],
+            'bio'                       =>      ['nullable' , 'string', 'max:2000'],
+            'subjects'                  =>      ['required' , 'array'],
+            'subjects.*'                =>      ['required' , 'numeric'],
+            'socialMedia'               =>      ['nullable' , 'array'],
+            'socialMedia.*'             =>      ['nullable' , 'url'],
+            'is_visible'                =>      ['required' , 'in:0,1'],
+            'price_per_minute'          =>      ['nullable' , 'numeric'],
+            'educational_background'    =>      ['nullable' , 'string' , 'max:600'],
+            'working_background'        =>      ['nullable' , 'string' , 'max:600'],
+            'first_day_of_work'         =>      ['nullable' , 'min:10' , 'max:10'],
+            //'avatar'                    =>      []
         ];
     }
 }
